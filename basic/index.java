b01a1394d81e73f36cc35a17e13fd17290bf3bb9
Sarthakrawt <155871591+Sarthakrawt@users.noreply.github.com>
2024-05-04 00:41:41 +0530

    Add files via upload

---
+++ basic/index.java
@@ -0,0 +1,10 @@
+package basic;
+
+// folder name where your file is located
+public class index {
+    public static void main(String[] args) {
+        // system is an class
+        // which have some properties like out and a function println()
+        System.out.println("hello world!");
+    }
+}
