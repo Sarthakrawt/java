7ebe956bc5e24c175a1c1a8963aa6394ed27018e
Sarthakrawt <155871591+Sarthakrawt@users.noreply.github.com>
2024-05-05 09:27:35 +0530

    Update functionoverloading.java

--- basic/Function/functionoverloading.java
+++ basic/Function/functionoverloading.java
@@ -1,14 +1,16 @@
+// function overloading  when we make a function
+//multiple times with same name but there paramenters are differnt
 public class functionoverloading {
     public static void main(String[] args) {
         // which function should be run decided in compile time which should be run
         fun(45);
         fun(12, "sarthak");
     }
-
+   // fun function with int a paramenter
     static void fun(int a) {
         System.out.println(a);
     }
-
+   // fun function with a , b paramenter
     static void fun(int a, String b) {
         System.out.println(a + " " + b);
     }
