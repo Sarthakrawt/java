20b0b9ecbbefe7d812314ae8612b3dffd3c09129
Sarthakrawt <155871591+Sarthakrawt@users.noreply.github.com>
2024-06-19 13:18:18 +0530

    Update basicbitOperator.java

--- basic2/BitwiseOperator/basicbitOperator.java
+++ basic2/BitwiseOperator/basicbitOperator.java
@@ -1,1 +1,27 @@
 
+public class BasicbitOperator {
+    public static void main(String[] args) {
+        // And bitwise Operator
+        // a b a&b
+        // ex 0 1 0
+        // a*b
+        int a = 4;
+        int b = 6;
+        System.out.println(a & b);
+
+        // OR bitwise operator
+        // a b aORb
+        // ex 0 1 1
+        // a+b
+        System.out.println(a | b);
+
+        // XOR bitwise operator
+        // if the values are same then it will give you 0
+        // a b a^b
+        // 0 0 0
+        // 1 1 0
+
+        System.out.println(a ^ b);
+
+    }
+}
